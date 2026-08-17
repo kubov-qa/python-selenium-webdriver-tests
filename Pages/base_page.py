e834c1ff670496d3f9d878bbf60e0f215c34f4de
Mitia <kubov.work@gmail.com>
2026-08-17 19:22:56 +0300

    BasePage is added

---
+++ Pages/base_page.py
@@ -0,0 +1,20 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+class BasePage:
+    
+    def __init__(self, driver, timeout=10):
+      self.driver = driver
+      self.timeout = timeout
+
+    
+    def find(self, locator):
+      element = WebDriverWait(self.driver, self.timeout).until(EC.visibility_of_element_located(locator))
+      return element
+
+    
+    def click(self, locator):
+      element = WebDriverWait(self.driver, self.timeout).until(EC.element_to_be_clickable(locator))
+      element.click()
+      
