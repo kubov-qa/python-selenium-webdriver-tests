3fb2ec62819a0f5c7884a3982410164d55c70455
Mitia <kubov.work@gmail.com>
2026-08-18 20:26:00 +0300

    test_data file is added and filled with constants

---
+++ Tests/test_data.py
@@ -0,0 +1,8 @@
+VALID_USERNAME = "standard_user"
+LOCKED_OUT_USERNAME = "locked_out_user"
+PROBLEM_USERNAME = "problem_user"
+PERFORMANCE_GLITCH_USERNAME = "performance_glitch_user"
+ERROR_USERNAME = "error_user"
+VISUAL_USERNAME = "visual_user"
+VALID_PASSWORD = "secret_sauce"
+
