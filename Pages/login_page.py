19ea4b24315a0b663f9d4689c842a83eabde2814
Mitia <kubov.work@gmail.com>
2026-08-19 16:36:39 +0300

    test with locked user is added

--- Pages/login_page.py
+++ Pages/login_page.py
@@ -5,6 +5,8 @@ class LoginPage(BasePage):
     USERNAME_INPUT = (By.CSS_SELECTOR, '[data-test="username"]')
     PASSWORD_INPUT = (By.CSS_SELECTOR, '[data-test="password"]')
     LOGIN_BUTTON = (By.CSS_SELECTOR, '[data-test="login-button"]')
+    ERROR_MESSAGE = (By.CSS_SELECTOR, '[data-test="error"]')
+    LOCKED_OUT_ERROR_TEXT = "Epic sadface: Sorry, this user has been locked out."
 
     def login(self, username, password):
         self.type_text(self.USERNAME_INPUT, username)
--- Tests/test_login.py
+++ Tests/test_login.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from Pages.login_page import LoginPage
 from Pages.inventory_page import InventoryPage
-from Tests.test_data import VALID_USERNAME, VALID_PASSWORD, BASE_URL
+from Tests.test_data import VALID_USERNAME, LOCKED_OUT_USERNAME, VALID_PASSWORD, BASE_URL
 
 
 def test_successful_login():
@@ -13,5 +13,14 @@ def test_successful_login():
   inventory_page = InventoryPage(driver)
   assert inventory_page.text(InventoryPage.PRODUCTS_TITLE) == InventoryPage.PRODUCTS_TITLE_TEXT
   assert driver.current_url == InventoryPage.INVENTORY_URL
+
+  driver.quit()
+
+def test_locked_out_user_cannot_login():
+  driver = webdriver.Chrome()
+  login_page = LoginPage(driver)
+  login_page.open(BASE_URL)
+  login_page.login(LOCKED_OUT_USERNAME, VALID_PASSWORD)
+  assert login_page.text(LoginPage.ERROR_MESSAGE) == LoginPage.LOCKED_OUT_ERROR_TEXT
 
   driver.quit()
